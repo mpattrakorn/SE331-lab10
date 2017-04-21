473b92e02a8dd2f9289584b74d45fb12704db7a3
DESKTOP-9CKSS1Q\CAMT <mark29162@gmail.com>
2017-04-21 16:32:01 +0700

    v3

---
+++ src/main/java/camt/cbsd/config/WebSecurityConfig.java
@@ -0,0 +1,7 @@
+package camt.cbsd.config;
+
+/**
+ * Created by CAMT on 4/21/2017.
+ */
+public class WebSecurityConfig {
+}
---
+++ src/main/java/camt/cbsd/security/repository/AuthorityRepository.java
@@ -0,0 +1,7 @@
+package camt.cbsd.security.repository;
+
+/**
+ * Created by CAMT on 4/21/2017.
+ */
+public class AuthorityRepository {
+}
